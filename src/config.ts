1d6998686f24560168104f27f27555a9e849a2e4
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-01-24 00:57:54 +0000

    update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
+
 export const config = {
     BotToken: process.env.BotToken, // you can just replace that with your token
     BotPrefix: "u!",
