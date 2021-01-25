7fddfeed497abbb7d1e47529df522c40cc8f1a86
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2021-01-25 23:08:10 +0000

    whoops

--- src/Commands/help.ts
+++ src/Commands/help.ts
@@ -16,7 +16,7 @@ export const command = new Command(
             .setTitle('Schedule audio - Help')
             .addFields(
                 {name: "upload", value: `Uploads an audio to ROBLOX.\n\n**Arguments**: "name" "time in seconds epoch" "cookie" - **Requires a file attachment to be uploaded too**.\n\n**Example**: \`u!upload "test" "1611439243064" "cookie here"\`\n\n**Extra info**: You can use "now" for the time as well.`},
-                {name: "verify", value: `Verifies if a cookie is valid.\n\n**Arguments**: "cookie"\n\n**Example**: u!verify "cookie"`},
+                {name: "valid", value: `Verifies if a cookie is valid.\n\n**Arguments**: "cookie"\n\n**Example**: u!valid "cookie"`},
                 {name: "robux", value: `Shows how much robux an account has.\n\n**Arguments** "cookie"\n\n**Example**: u!robux "cookie"`}
             )
             .setTimestamp()
